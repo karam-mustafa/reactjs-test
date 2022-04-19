6e03744d71882d97c38b4c4f15797d54e94201e6
karam  mustafa <karam2mustafa@gmail.com>
2022-04-19 17:20:57 +0300

    wip

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Weather from "./Weather";
+
+class Form extends Component {
+
+    render() {
+        return (
+            <div>
+                Form
+            </div>
+        );
+    }
+}
+
+export default Form;
---
+++ src/components/Weather.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Weather extends Component {
+
+    render() {
+        return (
+            <div>
+                Weather
+            </div>
+        );
+    }
+}
+
+export default Weather;
