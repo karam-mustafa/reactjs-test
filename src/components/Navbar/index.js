7cd6c715ce6e02bb86c00e49eca42071f2158b78
karam  mustafa <karam2mustafa@gmail.com>
2022-04-23 22:23:36 +0300

    wip

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import './style.css'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <h2 className="logo-text">Ultra Profile</h2>
+                </div>
+                <ul className="ul-list">
+                    <li className="list-item"><a href="#">Home</a></li>
+                    <li className="list-item"><a href="#">Work</a></li>
+                    <li className="list-item"><a href="#">Portfolio</a></li>
+                    <li className="list-item"><a href="#">Resume</a></li>
+                    <li className="list-item"><a href="#">About</a></li>
+                    <li className="list-item"><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </div>
     )
 };
