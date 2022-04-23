29837f7933b8b8df0026dc94c2e0809f995ad030
karam  mustafa <karam2mustafa@gmail.com>
2022-04-23 15:59:24 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import './App.css';
 import React from "react";
-import Form from "./components/Form";
-import Weather from "./components/Weather";
-
-const API_KEY = "86fc204f6e4391fa145c74b97a5f2865";
 
 class App extends React.Component {
-    state = {
-        city: "",
-        country: "",
-        hum: "",
-        description: "",
-        error: "",
-        temp: "",
-    };
-    getWeather = async (e) => {
-        e.preventDefault();
-        const city = e.target.elements.city.value;
-        const country = e.target.elements.country.value;
-        const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`);
-        const data = await api.json();
-        if (city && country) {
-            this.setState({
-                city: data.name,
-                country: data.sys.country,
-                hum: data.main.humidity,
-                description: data.weather[0].description,
-                error: "",
-                temp: data.main.temp,
-            });
-        } else {
-            this.setState({
-                error: "please insert valid data"
-            })
-        }
-    };
 
     render() {
         return (
             <div className="App">
-                <Form getWeather={this.getWeather}/>
-                <Weather data={this.state}/>
+
             </div>
         );
     }
