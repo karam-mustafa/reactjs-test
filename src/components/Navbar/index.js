97bc713d4bdb7d10e0e152074ff337bf807cd456
karam  mustafa <karam2mustafa@gmail.com>
2022-04-23 22:28:10 +0300

    wip

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './style.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -9,12 +10,12 @@ const Navbar = () => {
                     <h2 className="logo-text">Ultra Profile</h2>
                 </div>
                 <ul className="ul-list">
-                    <li className="list-item"><a href="#">Home</a></li>
+                    <li className="list-item"><NavLink to="/">Home</NavLink></li>
                     <li className="list-item"><a href="#">Work</a></li>
                     <li className="list-item"><a href="#">Portfolio</a></li>
                     <li className="list-item"><a href="#">Resume</a></li>
                     <li className="list-item"><a href="#">About</a></li>
-                    <li className="list-item"><a href="#">Contact</a></li>
+                    <li className="list-item"><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </div>
