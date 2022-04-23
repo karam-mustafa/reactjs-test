793a5157e8447673ecad5cc2a2f13d2a4b0d372d
karam  mustafa <karam2mustafa@gmail.com>
2022-04-23 16:01:35 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import React from "react";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Work from "./components/Work";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import About from "./components/About";
+import SocialMedia from "./components/SocialMedia";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
 
     render() {
         return (
             <div className="App">
-
+                <Navbar/>
+                <Home/>
+                <Work/>
+                <Portfolio/>
+                <Profile/>
+                <About/>
+                <SocialMedia/>
+                <Footer/>
             </div>
         );
     }
