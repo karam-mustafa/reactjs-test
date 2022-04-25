2a033a1553a142dfc47acccc463a3edadb0234bd
karam  mustafa <karam2mustafa@gmail.com>
2022-04-25 15:03:02 +0300

    wip

---
+++ src/components/Home/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HomeComponent = styled.div`
+    height: 500px;
+    background: url('../images/Home/home-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    text-align: center;
+    position: relative;
+}`
