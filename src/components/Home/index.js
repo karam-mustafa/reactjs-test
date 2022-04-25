bafd0813fa6839a8d7a3bffcc1b2f61095e63dde
karam  mustafa <karam2mustafa@gmail.com>
2022-04-25 15:02:44 +0300

    wip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,23 @@
 import React from "react";
+import './style.css';
+import {HomeComponent} from "./style";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <HomeComponent>
+            <div className="container">
+                <div className="home-information">
+                    <h2 className="home-title margin-bottom">Karam Mustafa</h2>
+                    <h4 className="home-info">Creative Director</h4>
+                    <p className="home-desc">
+                        Iam a professional <span>UX Designer</span> and Front-End Developer creating modern and
+                        resposive designs to Web and Mobile. Let us work together. Thank you.
+                    </p>
+                    <button className="home-btn">Let's Begin</button>
+                </div>
+            </div>
+        </HomeComponent>
+
     )
 };
 
