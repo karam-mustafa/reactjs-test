b56ec3e7f5fda176f24b557b7b11d93c0aac7065
karam  mustafa <karam2mustafa@gmail.com>
2022-04-26 23:00:07 +0300

    wip

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -2,8 +2,32 @@ import React from "react";
 
 const SocialMedia = () => {
     return (
-        <div>
-            SocialMedia
+        <div className="social-media">
+
+            <div className="social face">
+                <i className="icon fa fa-facebook fa-lg"></i>
+                <p>
+                    <span className="info1">Follow Me on</span>
+                    <span className="info2">Social Facebook</span>
+                </p>
+            </div>
+
+            <div className="social twitter">
+                <i className="icon fa fa-twitter fa-lg"></i>
+                <p>
+                    <span className="info1">Tweet Me on</span>
+                    <span className="info2">Social twitter</span>
+                </p>
+            </div>
+
+            <div className="social pin">
+                <i className="icon fa fa-pinterest fa-lg"></i>
+                <p>
+                    <span className="info1">Pin Me on</span>
+                    <span className="info2">Social Pinterest</span>
+                </p>
+            </div>
+
         </div>
     )
 };
