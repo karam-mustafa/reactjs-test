d219d8e49df007508056bb1eedefd9eab472494c
karam  mustafa <karam2mustafa@gmail.com>
2022-05-04 23:38:44 +0300

    wip

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className="drop">
+            <div className="container">
+                <h2 className="drop-title"><span>Drop </span>Me A line</h2>
+                <form action="">
+                    <div className="form-input">
+                        <input type="text" placeholder="Your Name"/>
+                        <input type="email" placeholder="Your Email"/>
+                    </div>
+                    <input type="text" className="sub" placeholder="Your Subject"/>
+                    <textarea cols="30" rows="10" placeholder="Your Message"/>
+                    <input type="submit" value="Send Message"/>
+                </form>
+            </div>
         </div>
     )
 };
