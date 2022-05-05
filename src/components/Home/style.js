6ae075fdca5b4c945086552ac5ae645616aa5497
karam  mustafa <karam2mustafa@gmail.com>
2022-05-05 15:35:34 +0300

    wip

--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -7,4 +7,17 @@ export const HomeComponent = styled.div`
     background-position: center;
     text-align: center;
     position: relative;
-}`
+}`;
+export const InformationComponent = styled.div`
+    height: 500px;
+    background: url('../images/Home/home-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    text-align: center;
+    position: relative;
+}`;
+export const ContainerComponent = styled.div`
+ width: 85%;
+ margin: auto;
+
+}`;
