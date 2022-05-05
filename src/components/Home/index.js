6eb648e6e470667541a41c566d1273610662d364
karam  mustafa <karam2mustafa@gmail.com>
2022-05-05 15:37:00 +0300

    wip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './style.css';
-import {HomeComponent} from "./style";
+import {ContainerComponent, HomeComponent, InformationComponent} from "./style";
 
 const Home = () => {
     return (
         <HomeComponent>
             <ContainerComponent>
-                <div className="home-information">
+                <InformationComponent>
                     <h2 className="home-title margin-bottom">Karam Mustafa</h2>
                     <h4 className="home-info">Creative Director</h4>
                     <p className="home-desc">
@@ -14,7 +14,7 @@ const Home = () => {
                         resposive designs to Web and Mobile. Let us work together. Thank you.
                     </p>
                     <button className="home-btn">Let's Begin</button>
-                </div>
+                </InformationComponent>
             </ContainerComponent>
         </HomeComponent>
 
