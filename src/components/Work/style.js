45378cc0c337a0bd1d567f9a2807b1792c06cd37
karam  mustafa <karam2mustafa@gmail.com>
2022-05-06 18:17:08 +0300

    wip

--- src/components/Work/style.js
+++ src/components/Work/style.js
@@ -1,8 +1,37 @@
 import styled from 'styled-components';
 
-export const WorkComponent  = styled.div`
+export const Work  = styled.div`
     height: auto;
     padding: 50px 0;
     overflow: hidden;
     background: #fff
+`;
+export const Title  = styled.h2`
+    font-size: 60px;
+`;
+
+export const Span  = styled.h2`
+    font-weight: normal
+`;
+
+export const Part  = styled.div`
+    margin-top: 20px;
+    width: 30%;
+    float: left;
+    height: auto;
+    padding: 100px 0;
+    border: 1px solid #888;
+    box-sizing: border-box;
+    text-align: center;
+    margin-left: 5%
+`;
+
+export const Icon  = styled.i`
+   color: #888;
+    margin-bottom: 20px
+`;
+
+export const PartTitle  = styled.h4`
+   color: #888;
+    margin-bottom: 20px
 `;
