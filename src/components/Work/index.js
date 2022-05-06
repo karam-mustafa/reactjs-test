73d98abdd81ceff95bf323f8d9f69a4cf32b3905
karam  mustafa <karam2mustafa@gmail.com>
2022-05-06 18:25:08 +0300

    wip

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,42 +1,24 @@
 import React from "react";
 import './style.css';
-import {WorkComponent} from "./style";
+import {Icon, Line, Part, PartDesc, PartTitle, Span, Title, Work} from "./style";
+import {ContainerComponent} from "../Home/style";
 
 const Work = () => {
     return (
-        <WorkComponent>
-            <div className="container">
-                <h2 className="work-title"><span>My</span> Work</h2>
-                <div className="part first">
-                    <i className="icon fa fa-chain fa-2x"/>
-                    <h4 className="part-title">Mobile Ux</h4>
-                    <hr className="line" />
-                        <p className="part-desc">
+        <Work>
+            <ContainerComponent>
+                <Title><Span>My</Span> Work</Title>
+                <Part>
+                    <Icon className="icon fa fa-chain fa-2x"/>
+                    <PartTitle>Mobile Ux</PartTitle>
+                    <Line/>
+                        <PartDesc>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quos omnis voluptas ea
                             ipsam! Voluptas.
-                        </p>
-                </div>
-
-                <div className="part">
-                    <i className="icon fa fa-bolt fa-2x"/>
-                    <h4 className="part-title">Mobile Ux</h4>
-                    <hr className="line" />
-                        <p className="part-desc">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quos omnis voluptas ea
-                            ipsam! Voluptas.
-                        </p>
-                </div>
-                <div className="part last">
-                    <i className="icon fa fa-tachometer fa-2x"/>
-                    <h4 className="part-title">Mobile Ux</h4>
-                    <hr className="line" />
-                        <p className="part-desc">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quos omnis voluptas ea
-                            ipsam! Voluptas.
-                        </p>
-                </div>
-            </div>
-        </WorkComponent>
+                        </PartDesc>
+                </Part>
+            </ContainerComponent>
+        </Work>
     )
 };
 
