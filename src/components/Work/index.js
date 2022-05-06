be6f311fd04830046e7970068310b7990d3a8f51
karam  mustafa <karam2mustafa@gmail.com>
2022-05-06 18:29:50 +0300

    wip

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,25 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 import './style.css';
 import {Icon, Line, Part, PartDesc, PartTitle, Span, Title, WorkComponent} from "./style";
 import {ContainerComponent} from "../Home/style";
 
-const Work = () => {
-    return (
-        <WorkComponent>
-            <div className='container'>
-                <Title><Span>My</Span> Work</Title>
-                <Part first={1}>
-                    <Icon className="icon fa fa-chain fa-2x"/>
-                    <PartTitle>Mobile Ux</PartTitle>
-                    <Line/>
+class  Work extends Component {
+    render() {
+        return (
+            <WorkComponent>
+                <div className='container'>
+                    <Title><Span>My</Span> Work</Title>
+                    <Part first={1}>
+                        <Icon className="icon fa fa-chain fa-2x"/>
+                        <PartTitle>Mobile Ux</PartTitle>
+                        <Line/>
                         <PartDesc>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quos omnis voluptas ea
                             ipsam! Voluptas.
                         </PartDesc>
-                </Part>
-            </div>
-        </WorkComponent>
-    )
-};
+                    </Part>
+                </div>
+            </WorkComponent>
+        );
+    }
+}
 
 export default Work;
