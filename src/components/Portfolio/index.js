0cb23691ffaf53def31b028a34612e3b31d8ba4a
karam  mustafa <karam2mustafa@gmail.com>
2022-05-07 15:33:30 +0300

    wip

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './style.css'
 import {Box, Image, ImageWrapper, List, ListItem, Overlay, OverlaySpan, PortfolioSection, Span, Title} from "./style";
+import axios from "axios";
 
 const Portfolio = () => {
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        axios.get('./js/data.json').then(res => setImages(res.data.portfolio))
+    }, []);
+
+    const data = images.map(item => {
+        return (
+            <ImageWrapper key={item.id}>
+                <Image src={item.image} alt=""/>
+                <Overlay>
+                    <OverlaySpan>
+                        Show Image
+                    </OverlaySpan>
+                </Overlay>
+            </ImageWrapper>
+        );
+    });
+
     return (
         <PortfolioSection>
             <Title><Span>My</Span> Portfolio</Title>
@@ -15,14 +36,7 @@ const Portfolio = () => {
             </List>
 
             <Box>
-                <ImageWrapper>
-                    <Image src="images/Portfolio/portfolio-img1.jpg" alt=""/>
-                    <Overlay>
-                        <OverlaySpan>
-                            Show Image
-                        </OverlaySpan>
-                    </Overlay>
-                </ImageWrapper>
+                {data}
             </Box>
         </PortfolioSection>
 
