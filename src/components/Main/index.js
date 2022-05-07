024374df7e6fdd8851fd3ae30a6ff07f01e9299b
karam  mustafa <karam2mustafa@gmail.com>
2022-05-07 22:38:19 +0300

    wip

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -14,7 +14,7 @@ class Main extends React.Component {
                 <Home/>
                 <Work/>
                 <Portfolio/>
-                <Profile/>
+                {/*<Profile/>*/}
                 <About/>
                 <SocialMedia/>
             </div>
